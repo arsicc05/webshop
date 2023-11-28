7753c75edc7ecb568c1a0da3d6b71bb7511f0779
Stefan Arsic <stefanarsic1201@gmail.com>
2023-11-28 19:08:52 +0100

    dodaj proizvod i o nama

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ function openForm() {
 function closeForm() {
   document.getElementById("popup").style.display = "none";
 }
+function openFormAddProducts() {
+  document.getElementById("add-products").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeFormAddProducts() {
+  document.getElementById("add-products").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
