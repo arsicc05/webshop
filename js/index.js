3a3501cb4e32ce6ae53138ac63d82803e9a7ecc2
Stefan Arsic <stefanarsic1201@gmail.com>
2024-01-13 14:38:30 +0100

    firebase

--- js/index.js
+++ js/index.js
@@ -9,8 +9,22 @@ function openFormAddProducts() {
   document.getElementById("add-products").style.display = "block";
   document.getElementById("overlay").style.display = "block";
 }
+function openFormEditProducts() {
+  document.getElementById("edit-products").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
 
 function closeFormAddProducts() {
   document.getElementById("add-products").style.display = "none";
   document.getElementById("overlay").style.display = "none";
+}
+function closeFormEditProducts() {
+  document.getElementById("edit-products").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+function openReg() {
+  registracija = document.getElementById("registracija");
+  prijava = document.getElementById("prijava");
+  registracija.style.display = "block";
+  prijava.style.display = "none";
 }
